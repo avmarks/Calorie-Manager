a47dfa7aa067688358cdcf86333120dbcf8cca93
Alex Marks <avrmarks@gmail.com>
2018-02-12 20:05:27 -0600

    Adding initial hibernate work.

---
+++ src/main/java/edu/matc/entity/User.java
@@ -0,0 +1,67 @@
+package edu.matc.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * This class represents a user
+ *
+ * @author Alex Marks
+ */
+@Entity(name = "User")
+@Table(name = "user")
+public class User {
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "user_name")
+    private String userName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
